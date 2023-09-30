5f124e7319293b7353a58909403ce4714dd60329
Benjamin Elenbaas <elenbaab@bc.edu>
2023-09-30 16:31:43 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,31 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello() -> str:
+    return render_template('home.html')
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+@app.route('/services')
+def services():
+    return render_template('services.html')
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+@app.route('/testfile')
+def testfile():
+    return render_template('testfile.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
